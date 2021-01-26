8cd3ec7ab16cc079f68fb3e6dac1b5b3a72bf13b
Gustavo Anjos <gustavoanjos42@gmail.com>
2021-01-26 14:44:17 -0400

    Initial Commit

---
+++ empresas_ios/empresas_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  empresas_iosApp.swift
+//  empresas_ios
+//
+//  Created by Gustavo Anjos on 26/01/21.
+//
+
+import SwiftUI
+
+@main
+struct empresas_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
